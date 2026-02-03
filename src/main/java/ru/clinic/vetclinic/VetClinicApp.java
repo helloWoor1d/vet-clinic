04cc81aed4a083c5451304c706e48e7892ad46e1
mira <pavuuuuuk@yandex.ru>
2026-02-03 22:35:45 +0300

    Initial commit

---
+++ src/main/java/ru/clinic/vetclinic/VetClinicApp.java
@@ -0,0 +1,13 @@
+package ru.clinic.vetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetClinicApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetClinicApp.class, args);
+    }
+
+}
---
+++ src/test/java/ru/clinic/vetclinic/VetClinicAppTests.java
@@ -0,0 +1,13 @@
+package ru.clinic.vetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
